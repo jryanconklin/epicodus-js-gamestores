86dbf4c96096cb363a19af499521c1889d9396c8
J. Ryan Conklin and ryan vinyard <j.@>
2016-10-12 08:55:55 -0700

    Add Initial Files and Directories.

---
+++ epicodus-commands.md
@@ -0,0 +1,63 @@
+# Run First:
+npm init
+bower init
+
+# Run Second:
+npm install browserify --save-dev
+npm install del --save-dev
+npm install jshint --save-dev
+npm install gulp --save-dev
+npm install gulp-concat --save-dev
+npm install gulp-jshint --save-dev
+npm install gulp-uglify --save-dev
+npm install gulp-util --save-dev
+npm install vinyl-source-stream --save-dev
+npm install browser-sync --save-dev
+
+//After Sass (See Below)
+npm install gulp-sass gulp-sourcemaps --save-dev
+
+bower install jquery --save
+bower install bootstrap --save
+npm install bower-files --save-dev
+
+# Global Installs - New Setup Only:
+
+npm install bower -g
+
+# Old Project on New Machine
+npm install //Check on this
+bower install
+
+# Gulp Script Tags
+
+
+# Bower Link/Script Tags
+
+## JavaScript - FIRST
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+
+## CSS - SECOND
+<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+
+# API's and Optional Installs
+bower install moment --save
+<script src="bower_components/moment/min/moment.min.js"></script>
+
+# Final Script and CSS Tags
+
+<link rel="stylesheet" href="build/css/vendor.css">
+<script src="build/js/vendor.min.js"></script>
+<script type="text/javascript" src="build/js/app.js"></script>
+
+# Install Ruby and Sass
+
+## Homebrew
+$ /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+## Ruby
+$ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+$ sudo gem install sass
---
+++ gulpfile.js
@@ -0,0 +1,120 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var browserify = require('browserify');
+var del = require('del');
+var source = require('vinyl-source-stream');
+var buildProduction = utilities.env.production;
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+// Bower Files for CSS - Additional Research Required
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap":{
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
+
+// JavaScript Server Sync
+var browserSync = require('browser-sync').create();
+
+// JSHint Function to Run JS Hint on All JS files in the JS Directory
+gulp.task('jshint', function() {
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// Concats JS Files => Preps for Browser => Cleans Old Folders => Minifies if Production
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('minifyScripts', ['jsBrowserify'], function(){
+  return gulp.src('./build/js/app.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('clean', function(){
+  return del(['build', 'tmp']);
+});
+
+gulp.task('build', ['clean'], function() {
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+  gulp.start('bower'); //Functions Once Bower Added Below
+  gulp.start('cssBuild');
+});
+
+// Bower Tasks
+gulp.task('bowerJS', function() {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('bowerCSS', function() {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
+//Server Tasks - Watch Files and Run Reload Tasks on Change
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+  gulp.watch(['*.html'], ['htmlBuild']);
+  gulp.watch(['scss/*.scss'], ['cssBuild']);
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function() {
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function() {
+  browserSync.reload();
+});
+
+gulp.task('htmlBuild', function() {
+  browserSync.reload();
+});
+
+//Sass
+gulp.task('cssBuild', function() {
+  return gulp.src(['scss/*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('.build/css'));
+});
